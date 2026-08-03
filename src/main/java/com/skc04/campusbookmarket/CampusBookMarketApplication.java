6e57d9ba0b9799245a82c522335e43fbaba1237f
midogi <skc04010@naver.com>
2026-08-03 16:59:38 +0900

    Initial commit

---
+++ src/main/java/com/skc04/campusbookmarket/CampusBookMarketApplication.java
@@ -0,0 +1,13 @@
+package com.skc04.campusbookmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusBookMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusBookMarketApplication.class, args);
+	}
+
+}

ef6192047a58224cbda6abe07cf693d6c7eb20e5
midogi <skc04010@naver.com>
2026-08-03 20:07:11 +0900

    Add trade post creation form

---
+++ src/main/java/com/skc04/campusbookmarket/web/post/form/PostCreateForm.java
@@ -0,0 +1,32 @@
+package com.skc04.campusbookmarket.web.post.form;
+
+public class PostCreateForm {
+
+    private String title;
+    private long price;
+    private String sellerName;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+}
